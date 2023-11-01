6c9717119b124094b0d39751cf84a27ec39263ec
seitamuro <seitamuro0210@gmail.com>
2023-11-01 23:41:18 +0900

    types.d.tsに画像ファイルの型を追加

--- src/types.d.ts
+++ src/types.d.ts
@@ -7,3 +7,24 @@ declare module "*.frag" {
   const content: string;
   export default content;
 }
+
+declare module "*.jpg" {
+  const content: string;
+  export default content;
+}
+declare module "*.png" {
+  const content: string;
+  export default content;
+}
+declare module "*.jpeg" {
+  const content: string;
+  export default content;
+}
+declare module "*.gif" {
+  const content: string;
+  export default content;
+}
+declare module "*.svg" {
+  const content: string;
+  export default content;
+}
